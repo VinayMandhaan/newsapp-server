b45eebe4b3290fceb0fbf5b3fa77536320f2bdc3
VinayMandhaan <vinaymandhaan29@gmail.com>
2023-04-30 20:43:25 +0500

    Cors and Server Setup

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+var cors = require("cors");
+const axios = require('axios');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use(
+    express.json({
+        extended: false,
+    })
+);
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server Started on Port ${PORT}`);
+});
+
+
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
+
+app.get('/news', async (req, res) => {
+    const config = {
+        method: 'get',
+        url: 'https://newsapi.org/v2/everything?apiKey=28d38df90e1741a5ba95d0a7d8a27054',
+        params: {
+            q: req.query.query,
+            language: req.query.lang
+        }
+    }
+    try {
+        const response = await axios(config);
+        return res.json(response.data)
+    } catch (error) {
+        res.status(500).send({ error: 'Something went wrong.' });
+    }
+})
